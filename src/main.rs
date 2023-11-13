495d328b2844d67b9294a13126d7dfd81d59f9a2
KavanPrice <kavanprice@gmail.com>
2023-11-13 23:51:57 +0000

    Added iterative graph expansion

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ mod load_env_vars;
 
 use crate::load_env_vars::EnvVars;
 use futures_util::TryStreamExt;
-use neo4rs;
-use neo4rs::{query, Graph, Query};
+use neo4rs::{query, Graph, Node, Query};
 use rspotify::clients::BaseClient;
 use rspotify::model::AlbumType::Album;
 use rspotify::model::{AlbumId, ArtistId, FullAlbum, FullArtist, SimplifiedAlbum};
@@ -32,49 +31,104 @@ async fn main() {
         .await
         .unwrap(),
     );
-
-    let artist_uris = HashMap::from([
-        ("radiohead", "spotify:artist:4Z8W4fKeB5YxbusRsdQVPb"),
-        ("taylor_swift", "spotify:artist:06HL4z0CvFAxyc27GXpf02"),
-        ("arctic_monkeys", "spotify:artist:7Ln80lUS6He07XvHI8qqHH"),
-        ("tyler_the_creator", "spotify:artist:4V8LLVI7PbaPR0K2TGSxFF"),
-        ("olivia_rodrigo", "spotify:artist:1McMsnEElThX1knmY4oliG"),
-        ("daft_punk", "spotify:artist:4tZwfgrHOc3mvqYlEYSvVi"),
-        ("snarky_puppy", "spotify:artist:7ENzCHnmJUr20nUjoZ0zZ1"),
-    ]);
+    let mut unexpanded_artist_uris = Arc::new(Mutex::new(HashMap::from([
+        (
+            String::from("spotify:artist:4Z8W4fKeB5YxbusRsdQVPb"),
+            String::from("radiohead"),
+        ),
+        (
+            String::from("spotify:artist:6styCzc1Ej4NxISL0LiigM"),
+            String::from("the_smile"),
+        ),
+        (
+            String::from("spotify:artist:7tA9Eeeb68kkiG9Nrvuzmi"),
+            String::from("atoms_for_peace"),
+        ),
+    ])));
 
     let mut query_counter = 1;
 
-    for (artist_name, uri) in artist_uris {
-        let artist_start = Instant::now();
-        let artist = get_artist_from_uri(&spotify_client, uri).await.unwrap();
-        let artist_albums = get_artist_albums(&spotify_client, &artist).await.unwrap();
+    for i in 0..2 {
+        expand_graph(
+            &spotify_client,
+            &graph,
+            &mut unexpanded_artist_uris,
+            &mut query_counter,
+        )
+        .await
+        .unwrap();
+    }
+}
 
+async fn expand_graph(
+    client: &ClientCredsSpotify,
+    graph: &Arc<Graph>,
+    unexpanded_uris: &mut Arc<Mutex<HashMap<String, String>>>,
+    query_counter: &mut i32,
+) -> Result<(), ClientError> {
+    let mut keys_to_remove: Vec<String> = Vec::new();
+
+    let mut unexpanded_uris_locked = unexpanded_uris.lock().unwrap();
+
+    for (uri, artist_name) in unexpanded_uris_locked.iter() {
+        let artist_start = Instant::now();
+        let artist = get_artist_from_uri(client, uri).await?;
+        let artist_albums = get_artist_albums(client, &artist).await.unwrap();
         let queries = construct_artist_queries(&artist, &artist_albums).await;
 
         for query in queries {
             let query_start = Instant::now();
-            let mut query_vec: Vec<Query> = Vec::new();
-            query_vec.push(query);
+            let query_vec: Vec<Query> = vec![query];
 
-            match execute_query(&graph, query_vec).await {
+            match execute_merge_query(graph, query_vec).await {
                 Err(e) => {
                     eprintln!("Error processing query: {}", e);
-                    println!("Error processing query {} in ", query_counter);
+                    println!("Error processing query {} in ", *query_counter);
                 }
                 Ok(()) => {
                     let query_duration = query_start.elapsed();
                     println!(
                         "Successfully processed query {} in time {:?}",
-                        query_counter, query_duration
+                        *query_counter, query_duration
                     )
                 }
             }
-            query_counter += 1;
+            *query_counter += 1;
         }
         let artist_duration = artist_start.elapsed();
         println!("Completed {} in time {:?}", artist_name, artist_duration);
+        keys_to_remove.push(uri.to_owned());
     }
+
+    let formatted_uris = format!(
+        "\'{}\'",
+        unexpanded_uris_locked
+            .keys()
+            .map(|uri| uri.to_owned())
+            .collect::<Vec<String>>()
+            .join("\', \'")
+    );
+
+    let mut remaining_artists_result = graph
+        .execute(query(&format!(
+            "MATCH (artist:Artist) WHERE NOT artist.uri IN [{}] RETURN artist",
+            formatted_uris
+        )))
+        .await
+        .unwrap();
+
+    while let Ok(Some(row)) = remaining_artists_result.next().await {
+        let node: Node = row.get("artist").unwrap();
+        let id = node.get::<String>("uri").unwrap();
+        let name = node.get::<String>("name").unwrap();
+        unexpanded_uris_locked.insert(id, name);
+    }
+
+    for key in keys_to_remove {
+        unexpanded_uris_locked.remove(&key);
+    }
+
+    Ok(())
 }
 
 async fn get_artist_from_uri(
@@ -121,18 +175,18 @@ async fn get_artist_albums(
     }
 }
 
-async fn construct_artist_queries(artist: &FullArtist, albums: &Vec<FullAlbum>) -> Vec<Query> {
+async fn construct_artist_queries(artist: &FullArtist, albums: &[FullAlbum]) -> Vec<Query> {
     let mut queries: Vec<Query> = Vec::new();
     let artist_id = artist.id.clone().to_string().replace('\'', "\\'");
     let artist_name = artist.name.clone().replace('\'', "\\'");
 
     queries.push(query(&format!(
-        "MERGE (artist:Artist {{id: '{}', name: '{}'}})",
+        "MERGE (artist:Artist {{uri: '{}', name: '{}'}})",
         artist_id, artist_name
     )));
     for genre in artist.genres.iter() {
         queries.push(query(&format!(
-            "MERGE (artist:Artist {{id: '{}', name: '{}'}})\
+            "MERGE (artist:Artist {{uri: '{}', name: '{}'}})\
             MERGE (genre:Genre {{name: '{}'}})\
             MERGE (artist)-[:genre]->(genre)",
             artist_id, artist_name, genre
@@ -143,14 +197,14 @@ async fn construct_artist_queries(artist: &FullArtist, albums: &Vec<FullAlbum>) 
         let album_name = album.name.clone().replace('\'', "\\'");
 
         queries.push(query(&format!(
-            "MERGE (artist:Artist {{id: '{}', name: '{}'}})\
-            MERGE (album:Album {{id: '{}', name: '{}'}})\
+            "MERGE (artist:Artist {{uri: '{}', name: '{}'}})\
+            MERGE (album:Album {{uri: '{}', name: '{}'}})\
             MERGE (artist)-[:released]->(album)",
             artist_id, artist_name, album_id, album_name
         )));
         for genre in album.genres.iter() {
             queries.push(query(&format!(
-                "MERGE (album:Album {{id: '{}', name: '{}'}})\
+                "MERGE (album:Album {{uri: '{}', name: '{}'}})\
                 MERGE (genre: Genre {{name: '{}'}})\
                 MERGE (album)-[:has_genre]->(genre)",
                 album.id, album.name, genre
@@ -158,7 +212,7 @@ async fn construct_artist_queries(artist: &FullArtist, albums: &Vec<FullAlbum>) 
         }
         for image in album.images.iter() {
             queries.push(query(&format!(
-                "MERGE (album:Album {{id: '{}', name: '{}'}})\
+                "MERGE (album:Album {{uri: '{}', name: '{}'}})\
                 MERGE (image: Image {{url: '{}'}})\
                 MERGE (album)-[:has_album_art]->(image)",
                 album_id, album_name, image.url
@@ -178,9 +232,9 @@ async fn construct_artist_queries(artist: &FullArtist, albums: &Vec<FullAlbum>) 
                 let track_artist_name = track_artist.name.clone().replace('\'', "\\'");
 
                 queries.push(query(&format!(
-                    "MERGE (artist:Artist {{id: '{}', name: '{}'}})\
-                    MERGE (album:Album {{id: '{}', name: '{}'}})\
-                    MERGE (track:Track {{id: '{}', name: '{}'}})\
+                    "MERGE (artist:Artist {{uri: '{}', name: '{}'}})\
+                    MERGE (album:Album {{uri: '{}', name: '{}'}})\
+                    MERGE (track:Track {{uri: '{}', name: '{}'}})\
                     MERGE (artist)-[:wrote]->(track)\
                     MERGE (album)-[:has_track]->(track)",
                     track_artist_id, track_artist_name, album_id, album_name, track_id, track_name
@@ -192,7 +246,10 @@ async fn construct_artist_queries(artist: &FullArtist, albums: &Vec<FullAlbum>) 
     queries
 }
 
-async fn execute_query(graph: &Graph, query: Vec<Query>) -> Result<(), Box<dyn std::error::Error>> {
+async fn execute_merge_query(
+    graph: &Graph,
+    query: Vec<Query>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let txn = graph.start_txn().await?;
     let result = txn.run_queries(query).await;
     if let Err(err) = result {
